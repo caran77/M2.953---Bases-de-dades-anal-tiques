1006181ef2c25d1defff3acb4c66bf65578eb95d
cagarcia <cagarcia@hotelbeds.com>
2019-03-02 10:30:01 +0100

    first version of the exercise 2

--- PAC1/pec1_ej2.sql
+++ PAC1/pec1_ej2.sql
@@ -13,7 +13,27 @@ order by CLI.client_name desc, CLI.city asc;
 --comanda diferent al preu establert en la Taula mestre de productes. 
 --La suma del valor de la línia de comanda es calcula multiplicant la quantitat per el preu
 
-
+select	sum(total_diferencia) total_diferencia,
+		count(product_code)		total_productes 
+from	(
+	with productes_amb_preu_diferent as (
+		select	PRO.product_code, PRO.price
+		from	sale.tb_product	PRO
+		where	exists (
+			select	1
+			from	sale.tb_order_line	LIN
+			where	LIN.product_code	=	PRO.product_code
+			 and	LIN.unit_price		!=	PRO.price
+		)
+	)
+		select	sum(LIN.quantity * (LIN.unit_price - PAPD.price)) total_diferencia, PAPD.product_code
+		from	productes_amb_preu_diferent	PAPD
+		join	sale.tb_order_line			LIN on (
+				PAPD.product_code	=	LIN.product_code
+		)
+		group by PAPD.product_code
+) total;
+				
 --Obtenir el Nom del producte i el seu preu, i el Nom de la categoria i el Nom de la subcategoria 
 --associats de tots els productes que existeixin en la Taula mestra de productes, 
 --i que no han estat demanats per clients el país dels quals d'origen és diferent a Polònia, 
@@ -83,7 +103,57 @@ with total_per_comanda as (
 	)	
 	order by CAT.category_name desc nulls last;
 	
--- Obtenir per a cada país del client, el nombre total de clients diferents i el nombre total de comandes per a cada país que la seva Data de comanda (order date) sigui anterior al 28 Febrer 2016 i on la ciutat de client sigui diferent a Bilbao, ordenat descendentment per nombre total de comandes i ascendentment per país.
+-- Obtenir per a cada país del client, el nombre total de clients diferents i el nombre total de comandes 
+-- per a cada país que la seva Data de comanda (order date) sigui anterior al 28 Febrer 2016 i 
+-- on la ciutat de client sigui diferent a Bilbao, ordenat descendentment per nombre total de comandes i ascendentment per país
 
--- Obtenir el Nom del client, el Nom del producte i el nombre de línies de comanda, d'aquelles comandes amb Dates de comanda (order date) en les quals s'ha realitzat un nombre total de comandes igual o superior a 4 (de tots els clients), i el nombre dels quals de línies de comanda per a aquest client i producte sigui solament 1, ordenat ascendentment per Nom de client i descendentment per Nom de producte.
+with total_ordres_per_client_i_pais as (
+	select	CLI.country, CLI.client_code, count(*) total
+	from	sale.tb_order ORD
+	join	sale.tb_client CLI on (
+		ORD.client_code		=	CLI.client_code
+	)
+	where	CLI.city	!=	'Bilbao'
+	 and	order_date	<	to_date('28/02/2016', 'dd/mm/yyyy')
+	group by CLI.country, CLI.client_code
+) 
+	select	country, 
+			count(client_code) total_clients,
+			sum(total) total_comandes			
+	from	total_ordres_per_client_i_pais
+	group by country
+	order by total_comandes desc, country asc;
 	
+
+-- Obtenir el Nom del client, el Nom del producte i el nombre de línies de comanda, 
+-- d'aquelles comandes amb Dates de comanda (order date) en les quals s'ha realitzat un nombre total de comandes 
+-- igual o superior a 4 (de tots els clients), i el nombre dels quals de línies de comanda per a aquest client 
+-- i producte sigui solament 1, ordenat ascendentment per Nom de client i descendentment per Nom de producte
+
+with dias_amb_4_o_mes_comandes as (
+	select	ORD.order_date, count(*)
+	from	sale.tb_order ORD
+	group by ORD.order_date
+	having	count(*) >= 4
+)
+	select	CLI.client_name,
+			PRO.product_name,
+			count(*) linees_de_comanda
+	from	dias_amb_4_o_mes_comandes	COM
+	join	sale.tb_order				ORD on (
+		COM.order_date		=	ORD.order_date
+	)
+	join	sale.tb_client 	CLI on (
+		ORD.client_code	=	CLI.client_code
+	) 
+	join	sale.tb_order_line	LIN on (
+		ORD.order_number = LIN.order_number
+	)
+	join	sale.tb_product PRO on (
+		LIN.product_code = PRO.product_code 
+	)
+	group by CLI.client_name,
+			PRO.product_name				
+	having count(*) = 1
+	order by CLI.client_name asc,
+			PRO.product_name desc;				
